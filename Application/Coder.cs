d21d8ee56ff00e1915d06e8269d2b5959810b4a7
Crisan Camelia <cameliadaniela97@gmail.com>
2020-02-06 20:49:49 +0200

    TOTP with OTP.NET

---
+++ Application/Coder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OtpNet;
+using System.Text;
+
+namespace Application
+{
+    public class Coder
+    {
+        private Totp Totp { set; get; }
+
+        public string Code(string id, DateTime time)
+        {
+            Totp = new Totp(Encoding.ASCII.GetBytes(id), mode: OtpHashMode.Sha256, step:30);
+
+            return Totp.ComputeTotp(time);
+        }
+        public bool Verify(string oneTimePass)
+        {
+            return  Totp.VerifyTotp(oneTimePass, out var _);
+        }
+       
+    }
+}
---
+++ OneTimePassword/Program.cs
@@ -0,0 +1,35 @@
+﻿using Application;
+using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneTimePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("ID:");
+                var id = Console.ReadLine();
+                var coder = new Coder();
+                var oneTimePass = coder.Code(id, DateTime.UtcNow);
+                Console.WriteLine("OTP:" + oneTimePass);
+                Console.WriteLine("Password:");
+                oneTimePass = Console.ReadLine();
+                var login = coder.Verify(oneTimePass);
+                if (login)
+                    Console.WriteLine("Login Succesfull");
+                else
+                    Console.WriteLine("Please request another password");
+            }
+        }
+
+    }
+}
